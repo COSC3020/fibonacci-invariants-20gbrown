d993e14f24a1dde8c7d5734b9c2e8ddcf06812d4
gbrown20 <gbrown20@DESKTOP-2J0M7L5.localdomain>
2023-09-27 17:14:17 -0600

    Work in progress

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
-function fib(n) {
-    return n;
+//Gage Brown 
+
+function fib(n, arr = {}) {
+    if ( n in arr) {
+        return arr[n];
+    }
+
+    if (n === 0) {
+        return 0;
+    }
+    else if (n === 1) {
+        return 1;
+    }
+    else {
+        const result = fib(n - 1, arr) + fib(n - 2, arr);//recursive call 
+        arr[n] = result;
+        return result;
+    }
 }
--- code.test.js
+++ code.test.js
@@ -4,6 +4,41 @@ const jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
 function fibTest(n) {
+    var fib_solns = [0, 1]; //intialize with [0,1]
+    if(n <= 1) return fib_solns.slice(0, n + 1); //return the first n + 1 elements 
+    //fib_solns[1] = 1;
+    //if(n == 1) return fib_solns;
+    for(var i = 2; i <= n; i++) fib_solns[i] =
+        fib_solns[i-1] + fib_solns[i-2];
+    return fib_solns;
+}
+
+const test = jsc.forall("nat", function (n) {
+const computedFibonacci = fib(n);
+const expectedFibonacci = fibTest(n);
+
+if (computedFibonacci.length !== expectedFibonacci.length) {
+  return false;
+}
+
+for (let i = 0; i < computedFibonacci.length; i++) {
+  if (computedFibonacci[i] !== expectedFibonacci[i]) {
+    return false;
+  }
+}
+
+return true;
+});
+
+jsc.assert(test);
+
+/*
+const fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync('code.js')+'');
+
+function fibTest(n) {
     var fib_solns = [0];
     if(n == 0) return fib_solns;
     fib_solns[1] = 1;
@@ -19,3 +54,4 @@ const test =
             JSON.stringify(fibTest(n));
     });
 jsc.assert(test);
+*/
